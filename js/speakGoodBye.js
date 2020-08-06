376b5607090860d0cfcf476e8667a9599e5753a5
JP Vendramini <37249457+jpvendramini@users.noreply.github.com>
2020-08-06 20:31:51 -0300

    Add files via upload

---
+++ js/speakGoodBye.js
@@ -0,0 +1,12 @@
+(function (window) {
+
+	var speakWord = "Good Bye";
+
+	var byeSpeaker = {
+		speak: function speak(name) {
+		  console.log(speakWord + " " + name);
+		}
+	}
+
+	window.byeSpeaker = byeSpeaker;
+})(window);
